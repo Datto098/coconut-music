646decef5a13fea661b07cb0a2cabefabe73f1dc
datto098 <dat61222@gmail.com>
2023-09-03 19:58:42 +0700

    fixing

--- src/components/404/NotFoundPage.tsx
+++ src/components/404/NotFoundPage.tsx
@@ -3,15 +3,13 @@ import Image from 'next/image';
 export default function NotFoundPage() {
 	return (
 		<div className='w-full h-[100vh] flex items-center justify-center'>
-			<div>
-				<Image
-					className='w-[400px] h-[400px]'
-					width={100}
-					height={100}
-					src='/coconut-mp3-404-white.svg'
-					alt='404 Not Found'
-				/>
-			</div>
+			<Image
+				className='w-[400px] h-[400px]'
+				width={100}
+				height={100}
+				src='/coconut-mp3-404-white.svg'
+				alt='404 Not Found'
+			/>
 		</div>
 	);
 }
