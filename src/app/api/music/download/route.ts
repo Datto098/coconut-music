02f184cc8c18139488040f81dcba61a9253f5f6a
datto098 <dat61222@gmail.com>
2023-09-06 21:22:33 +0700

    clear readme file

--- src/app/api/music/download/route.ts
+++ src/app/api/music/download/route.ts
@@ -4,7 +4,6 @@ import {NextRequest, NextResponse} from 'next/server';
 import ytdl from 'ytdl-core';
 
 export const dynamic = 'force-dynamic';
-
 export async function GET(request: NextRequest) {
 	try {
 		// Get youtube url
