dc30d398245dd566323b80bdf256778cadc5e52f
datto098 <dat61222@gmail.com>
2023-09-06 10:03:32 +0700

    download mp3 page

--- src/app/api/mp3/get-info/route.ts
+++ src/app/api/mp3/get-info/route.ts
@@ -1,6 +1,7 @@
 import {NextRequest, NextResponse} from 'next/server';
 import ytdl from 'ytdl-core';
 
+export const dynamic = 'force-dynamic';
 export async function GET(request: NextRequest) {
 	try {
 		// Get youtube url
